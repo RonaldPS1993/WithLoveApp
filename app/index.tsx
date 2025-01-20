47358e72ea166c3b55f751bb82b339eaed8561e1
RonaldPS1993 <ronaldpenasollet@gmail.com>
2025-01-20 01:07:32 -0500

    Full screen issue solved

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, View, Dimensions, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function Index() {
+const { width, height } = Dimensions.get("window");
+
+export default function Home() {
+  const insets = useSafeAreaInsets();
   return (
+      
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        style={styles.fullScreen}
+      >
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  fullScreen: {
+    ...StyleSheet.absoluteFillObject, // Covers the entire screen
+    backgroundColor: '#FFFFF7', 
+  },
+});
