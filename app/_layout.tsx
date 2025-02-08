62a53ecad9fd3ff71c48ec8ea80f2ed800d1b42f
RonaldPS1993 <ronaldpenasollet@gmail.com>
2025-02-07 20:43:09 -0500

    Slash screen added time

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,22 @@
 import { Stack } from 'expo-router/stack';
 import { Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState, useCallback } from "react"
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 
 export default function Layout() {
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000)
+  }, []);
+
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
